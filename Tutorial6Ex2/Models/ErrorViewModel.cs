dcb5afaaac3905cb79d987bd48b2bbe20de653bc
faizibnfarhan <muhammadfaizkhan20@gmail.com>
2024-11-26 15:43:15 +1100

    second commit

--- Tutorial6Ex2/Models/ErrorViewModel.cs
+++ Tutorial6Ex2/Models/ErrorViewModel.cs
@@ -1,4 +1,4 @@
-﻿namespace Tutorial6Ex2.Models;
+namespace Tutorial6Ex2.Models;
 
 public class ErrorViewModel
 {
@@ -6,4 +6,3 @@ public class ErrorViewModel
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
-
