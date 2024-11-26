50c6c3d61c01534c8793ecd776ebcb05a43eaa75
faizibnfarhan <muhammadfaizkhan20@gmail.com>
2024-11-26 15:28:36 +1100

    initial commit

---
+++ Tutorial6Ex2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Tutorial6Ex2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
